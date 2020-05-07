8c7c3e7c42a7c9ba5098ac153693d2f1681760be
sv3a <sveas93@gmail.com>
2020-05-07 10:03:26 +0200

    Made unbalance parameters optional

--- Disc.m
+++ Disc.m
@@ -3,20 +3,32 @@ classdef Disc < NodeElement
 % inertia as well as potential particle unbalance.
 
 properties
-  m;  % mass [kg]
-  iD; % transverse mass moment of inertia [kg*m^2]
-  iP; % polar mass moment of inertia [kg*m^2]
-  m0; % localized unbalance [kg]
-  e;  % eccentricity of unbalance [m]
+  m;            % mass [kg]
+  iD;           % transverse mass moment of inertia [kg*m^2]
+  iP;           % polar mass moment of inertia [kg*m^2]
+  m0;           % localized unbalance [kg]
+  e;            % eccentricity of unbalance [m]
+  u;            % combined unbalance [kg*m]
+  hasUnbalance; % unbalance flag
 end
 
 methods
-  function obj = Disc(mass, momentInert, momentPolar, m0, e)
+  function obj = Disc(mass, momentInert, momentPolar, varargin)
     obj.m  = mass;
     obj.iD = momentInert;
     obj.iP = momentPolar;
-    obj.m0 = m0;
-    obj.e  = e;
+    obj.hasUnbalance = false;
+
+    % Optional discrete unbalance parameters
+    if nargin == 4
+      obj.u = varargin{1};
+      obj.hasUnbalance = true;
+    elseif nargin == 5
+      obj.m0 = varargin{1};
+      obj.e  = varargin{2};
+      obj.u  = obj.m0*obj.e;
+      obj.hasUnbalance = true;
+    end
   end
 end
 end
